a94c45a2dcc64922ce8187458093ef3dd6b92a73
Kevin Rue <kevinrue@users.noreply.github.com>
2025-10-10 15:01:08 +0100

    add roxygen docstring

--- R/example.R
+++ R/example.R
@@ -1,4 +1,15 @@
-## example.R
+#' Example Function
+#' 
+#' Messages the user and returns the sum of `x`and `y`.
+#'
+#' @param x One number.
+#' @param y Another number.
+#'
+#' @returns The sum of `x`and `y`
+#' @export
+#'
+#' @examples
+#' my_bioc_function(2, 3)
 my_bioc_function <- function(x, y) {
   message("This is an example function")
   return(x + y)
