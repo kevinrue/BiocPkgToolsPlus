49fdb01bd17daadeb7e0925e1b2465111ba6213e
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2025-10-11 12:02:03 +0100

    heatmap_biocview_by_package

--- R/matrix.R
+++ R/matrix.R
@@ -17,3 +17,23 @@ biocview_by_package <- function(pkg_list) {
   }
   return(res_matrix)
 }
+
+heatmap_biocview_by_package <- function(matrix) {
+  # make a heatmap of term vs package
+  # optionally subset to certain package
+  stopifnot(requireNamespace("magick"))
+  storage.mode(matrix) <- "integer"
+  Heatmap(
+    matrix = matrix[singlecell_pkgs, ],
+    name = "has_view",
+    col = circlize::colorRamp2(
+      breaks = c(0, 1),
+      colors = c("white", "green"),
+    ),
+    use_raster = TRUE,
+    row_names_gp = gpar(fontsize = 2),
+    column_names_gp = gpar(fontsize = 2),
+    cluster_rows = TRUE,
+    cluster_columns = FALSE
+  )
+}
