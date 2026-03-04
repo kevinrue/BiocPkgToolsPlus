cdadf2b8af0ff632cce02fd95af3e9c1d6b77553
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2026-03-04 13:06:53 +0000

    list packages maintained and authored, by user

--- R/get_packages_by_author.R
+++ R/get_packages_by_author.R
@@ -15,7 +15,7 @@
 #' if you are making multiple calls.
 #' See vignette 'Optimisations' for a more comprehensive discussion and demonstration.
 #'
-#' @returns Character vector of package names.
+#' @returns A named list with two elements: `Maintainer` and `Author`.
 #' @export
 #' @importFrom BiocPkgTools biocPkgList
 #' @importFrom biocViews getSubTerms
@@ -43,10 +43,20 @@ get_packages_by_author <- function(author, role = c("contributor", "maintainer")
     stop("Maintainer role not supported yet")
   }
   pkg_list <- .check_or_get_pkg_list2(pkg_list)
+  pkgs_authored <- .get_authored_packages(author, pkg_list)
+  pkgs_maintained <- .get_maintained_packages(author, pkg_list)
+  res <- list(
+    Maintainer = pkgs_maintained,
+    Author = pkgs_authored
+  )
+  return(res)
+}
+
+.get_authored_packages <- function(author, pkg_list) {
   test_one_authorset <- function(authors, name) {
     any(authors == name)
   }
-  test_one_name <- function(name, pkglist) {
+  test_one_name <- function(name, pkg_list) {
     vapply(
       X = pkg_list$Author,
       FUN = test_one_authorset,
@@ -57,9 +67,34 @@ get_packages_by_author <- function(author, role = c("contributor", "maintainer")
   res_pkg_by_alias <- vapply(
     X = author,
     FUN = test_one_name,
-    FUN.VALUE = logical(nrow(pkg_list))
+    FUN.VALUE = logical(nrow(pkg_list)),
+    pkg_list = pkg_list
   )
   which_pkgs <- which(rowSums(res_pkg_by_alias) > 0)
   res_pkgs <- pkg_list$Package[which_pkgs]
   return(res_pkgs)
 }
+
+.get_maintained_packages <- function(author, pkg_list) {
+  test_one_maintainer <- function(maintainer, name) {
+    any(grepl(name, maintainer))
+  }
+  test_one_name <- function(name, pkg_list) {
+    vapply(
+      X = pkg_list$Maintainer,
+      FUN = test_one_maintainer,
+      name = name,
+      FUN.VALUE = logical(1)
+    )
+  }
+  res_pkg_by_alias <- vapply(
+    X = author,
+    FUN = test_one_name,
+    FUN.VALUE = logical(nrow(pkg_list)),
+    pkg_list = pkg_list
+  )
+  which_pkgs <- which(rowSums(res_pkg_by_alias) > 0)
+  res_pkgs <- pkg_list$Package[which_pkgs]
+  return(res_pkgs)
+}
+
